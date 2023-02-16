e244a54345beb529597fb98d1c5a87e60f7caebd
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2023-02-16 12:07:40 +0100

    perro huye de las ratas como una ratilla jaja

--- Proyecto/IAV-P1/Assets/Scripts/Comportamientos/Llegada.cs
+++ Proyecto/IAV-P1/Assets/Scripts/Comportamientos/Llegada.cs
@@ -3,7 +3,7 @@
    http://www.federicopeinado.com
 
    Este fichero forma parte del material de la asignatura Inteligencia Artificial para Videojuegos.
-   Esta asignatura se imparte en la Facultad de Informática de la Universidad Complutense de Madrid (España).
+   Esta asignatura se imparte en la Facultad de Informï¿½tica de la Universidad Complutense de Madrid (Espaï¿½a).
 
    Autor: Federico Peinado 
    Contacto: email@federicopeinado.com
@@ -18,7 +18,7 @@ namespace UCM.IAV.Movimiento
     public class Llegada : ComportamientoAgente
     {
         /// <summary>
-        /// Obtiene la dirección
+        /// Obtiene la direcciï¿½n
         /// </summary>
         /// <returns></returns>
 
@@ -28,6 +28,9 @@ namespace UCM.IAV.Movimiento
         // El radio para llegar al objetivo
         public float rObjetivo;
 
+        //Rat number
+        [SerializeField]
+        private int ratNumber = 0;
         // El radio en el que se empieza a ralentizarse
 
         //Tiene que acceder a la lista de entidades para que el objetivo a seguir sea la ultima rata en vez del avatar
@@ -45,10 +48,12 @@ namespace UCM.IAV.Movimiento
 
             float targetSpeed = maxSpeed;
 
-            var dir = objetivo.transform.position - this.transform.position;
+            var dir = Vector3.zero;
+            if (ratNumber <= 2) dir = objetivo.transform.position - this.transform.position;
+            else dir = this.transform.position - lastRatPos;
             var dist = dir.magnitude;
 
-            if (dist < distancia)
+            if (ratNumber <= 2 && dist < distancia)
             {
                 return resultado;
             }
@@ -75,6 +80,25 @@ namespace UCM.IAV.Movimiento
             resultado.angular = 0;
             return resultado;
 
+        }
+
+        //private Collider senser = gameObject.GetComponentInChildren<Collider>();
+
+        private Vector3 lastRatPos;
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Rat")){
+                ratNumber++;
+                lastRatPos = other.gameObject.transform.position;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Rat")){
+                ratNumber--;
+            }
         }
 
 
